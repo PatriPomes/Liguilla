c915d8d212849ff3645f19192af2d38c2b3f0030
PatriPomes <pmanzanastomas@gmail.com>
2023-11-15 15:16:56 +0100

    vista del home preparada

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function __invoke(){ //adiminstrara una unica ruta, la pantalla de inicio.
-        return "Bienvenidos a nuestra liguilla!!!!";
+        return view('home');
     }
 }
